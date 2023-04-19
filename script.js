42c8f1e255f785870cef0a4275586d53d2f4e1d9
ribeiroAllison <allisonribeiro@gmail.com>
2023-04-19 14:15:38 -0300

    add remove chore button

--- script.js
+++ script.js
@@ -26,18 +26,24 @@ async function getList(){
 async function run() {
     const table = document.getElementById('toDoList')
     const result = await getList();
-    console.log(result)
+    
     for(chore of result){
         const newRow = document.createElement('tr');
         const idData = document.createElement('td');
         const choreData = document.createElement('td');
         const statusData = document.createElement('td');
+        const removeChore = document.createElement('td');
+        const removeChoreButton = document.createElement('button');
+        removeChoreButton.setAttribute('id', chore['id']);
+        removeChoreButton.innerHTML = 'Remover Tarefa';
+        removeChore.append(removeChoreButton);
         statusData.innerHTML = chore['finished'] === true ? 'resolvido' : 'pendente';
         idData.innerHTML = chore['id'];
         choreData.innerHTML = chore['chores']
         newRow.append(idData);
         newRow.append(choreData);
         newRow.append(statusData);
+        newRow.append(removeChore);
         table.append(newRow);
         
     }
@@ -66,7 +72,7 @@ function postNewChore(){
         },
         error: function(response){
             console.log(response)
-            console.log(data)
+            
         }
     });
 }
